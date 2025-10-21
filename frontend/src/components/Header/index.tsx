4b94dd26a9e53f441ffb6dae413093daf05c2d85
Ezequiel-Manfredi <ezemanfre@gmail.com>
2025-10-21 19:08:46 -0300

    add: header para inicio y cierre de sesión

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { useAuth } from "react-oidc-context";
+import { useLocation } from "react-router-dom";
+import UserModal from "./UserModal";
+import ShopCartModal from "./ShopCartModal";
+
+export default function Header() {
+    const auth = useAuth();
+    const path = useLocation().pathname
+    useEffect(()=> {
+        if (path !== '/login/callback') {
+            localStorage.setItem('redirect_path',path)
+        }
+    },[path])
+
+    return <header className="flex justify-between items-center p-2 pl-4 pr-4 border-b-2">
+        <div><a href="/" className="flex gap-1 justify-between items-center">
+            <img className="size-20" src="/shipper-logo.png"/>
+            <span>Shipper</span>
+        </a></div>
+        <div className="flex items-center gap-4">
+            {
+                !auth.isAuthenticated
+                ? <button 
+                    className="bg-white cursor-pointer p-2 text-black rounded" 
+                    onClick={() => {auth.signinRedirect()}}
+                >
+                    Iniciar Sesión
+                </button>
+                : <>
+                    <ShopCartModal/>
+                    <UserModal/>
+                </>
+            }
+        </div>
+    </header>
+}
